48a6daa8a375087ea8a8e64a124e8fa7a769c0df
John Raymond Ilagan <mellamoraymond@gmail.com>
2020-09-20 21:20:01 +0800

    Initial Commit

---
+++ Organika/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Organika
+//
+//  Created by Engr. John Raymond Ilagan on 9/20/20.
+//  Copyright © 2020 John Raymond Ilagan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
